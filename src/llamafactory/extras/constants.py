745a6c25fe4dbdfb5644b7a4e0a8acdb583cd026
octo-patch <266937838+octo-patch@users.noreply.github.com>
2026-09-02 00:05:24 +0800

    fix: address MiniMax model review feedback

--- src/llamafactory/extras/constants.py
+++ src/llamafactory/extras/constants.py
@@ -1111,10 +1111,6 @@ register_model_group(
             DownloadSource.DEFAULT: "MiniMaxAI/MiniMax-M2.7",
             DownloadSource.MODELSCOPE: "MiniMaxAI/MiniMax-M2.7",
         },
-        "MiniMax-M3-Thinking": {
-            DownloadSource.DEFAULT: "MiniMaxAI/MiniMax-M3",
-            DownloadSource.MODELSCOPE: "MiniMaxAI/MiniMax-M3",
-        },
     },
     template="minimax2",
 )
--- tests/extras/test_model_registry.py
+++ tests/extras/test_model_registry.py
@@ -1,10 +1,23 @@
+# Copyright 2026 the LlamaFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from llamafactory.extras.constants import SUPPORTED_MODELS, DownloadSource
 
 
 def test_minimax_models_are_registered() -> None:
     expected_models = {
         "MiniMax-M2.7-Thinking": "MiniMaxAI/MiniMax-M2.7",
-        "MiniMax-M3-Thinking": "MiniMaxAI/MiniMax-M3",
     }
 
     for model_name, model_path in expected_models.items():
